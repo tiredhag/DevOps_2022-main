4aaf023b5e79cefafcbbfb2e0f800015685c1195
Tired Hag <77437911+tiredhag@users.noreply.github.com>
2022-12-12 05:55:51 +0100

    Commented out failing s3 :pencil2:

--- infra/databucket.tf
+++ infra/databucket.tf
@@ -1,5 +1,6 @@
 # Jim; this just fails ... commented it out ! We need to figure this out later, starting new task instead...
-
+/*
 resource "aws_s3_bucket" "analyticsbucket" {
   bucket = "analytics-${var.candidate_id}"
 }
+*/
--- infra/provider.tf
+++ infra/provider.tf
@@ -3,11 +3,11 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "4.40.0"
-    }
+    }/*
     backend "s3" {
       bucket = "analytics-${var.candidate_id}"
       key = "<key>/apprunner-lab.state"
       region = "eu-north-1"
-    }
+    } */
   }
 }
